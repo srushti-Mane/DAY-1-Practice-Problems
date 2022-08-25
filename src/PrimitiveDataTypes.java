fb966d5353faa13a6832049a04c8fd572eaed355
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-08-25 09:06:10 +0530

    modified:logic for primitive data tyes

---
+++ src/PrimitiveDataTypes.java
@@ -0,0 +1,20 @@
+public class PrimitiveDataTypes {
+        static byte value1;
+        static short value2;
+        static int value3;
+        static long value4;
+        static float value5;
+        static double value6;
+        static boolean value7;
+        static char value8;
+    public static void main(String [] args) {
+        System.out.println("byte= "+ value1);
+        System.out.println("short= "+ value2);
+        System.out.println("int= "+ value3);
+        System.out.println("long= "+ value4);
+        System.out.println("float= "+ value5);
+        System.out.println("double= "+ value6);
+        System.out.println("boolean= "+ value7);
+        System.out.println("char= "+ value8);
+    }
+}
