9c3478c87b3c2dedf4fee6e3a290651891cdfd0c
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-08-25 09:15:02 +0530

    modified:logic for string is equal or not

---
+++ src/Strings_equal_not.java
@@ -0,0 +1,9 @@
+public class Strings_equal_not {
+    public static void main(String[] args){
+    String s1 = "srushti";
+    String s2 = "srushti";
+    String s3 = "srushti mane";
+    System.out.println(s1.equals(s2));
+    System.out.println(s2.equals(s3));
+}
+}
